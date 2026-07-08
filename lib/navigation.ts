0fbe56df98f1728500424f1fe0b9d0bc866fa833
Jaspal Singh <samplay1@gmail.com>
2026-07-08 22:24:53 +0530

    v0.1.3 Add navigation types and data

---
+++ lib/navigation.ts
@@ -0,0 +1,24 @@
+import { NavigationItem } from "@/types/navigation";
+
+export const navigation: NavigationItem[] = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Categories",
+    href: "/categories",
+  },
+  {
+    title: "Collections",
+    href: "/collections",
+  },
+  {
+    title: "Blog",
+    href: "/blog",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+];
