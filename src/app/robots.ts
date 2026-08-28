c388eb8535c07d22cd30fc6e10e4659198bca331
js91872 <samplay1@gmail.com>
2026-08-28 07:10:56 +0530

    feat(seo): generate discovery catalogs and crawl routes

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,10 @@ import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/'],
-    },
+    rules: [
+      { userAgent: '*', allow: ['/', '/llms.txt', '/tools.json'], disallow: ['/api/', '/debug', '/search'] },
+      { userAgent: ['GPTBot', 'Google-Extended', 'ClaudeBot', 'PerplexityBot', 'CCBot'], allow: ['/', '/llms.txt', '/tools.json'], disallow: ['/api/', '/debug', '/search'] },
+    ],
     sitemap: 'https://navorika.com/sitemap.xml',
   };
 }
